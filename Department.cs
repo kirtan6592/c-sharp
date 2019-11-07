10fc5e392091203b248d1288ab485eeb4b36a83a
kirtan.chhatbar <kirtan.chhatbar@DSK-889>
2019-11-07 12:56:01 +0530

    committing changes related to code first approch.

---
+++ Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public List<EmployeeList> Employees { get; set; }
+    }
+
+    public class EmployeeList
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Salary { get; set; }
+        public Department Department { get; set; }
+    }
+}
